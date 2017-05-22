6c22ca5403c84efa36208d7be42237d2fa280335
Colin Brinton <brintonc@seattleu.edu>
2017-05-22 16:52:26 -0700

    Initial  components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BrowseComponent } from './browse/browse.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', component: AppComponent },
+  { path: 'browse', component: BrowseComponent },
 ];
 
 @NgModule({
